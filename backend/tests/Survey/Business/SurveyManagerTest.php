5328d8b67f0fef0fed5409f4b8dc1163421d865f
hrakotobe <hrakotobe@lafourchette.com>
2018-02-10 11:27:00 +0100

    business classes tests

--- backend/tests/Survey/Business/SurveyManagerTest.php
+++ backend/tests/Survey/Business/SurveyManagerTest.php
@@ -23,6 +23,47 @@ class SurveyManagerTest extends TestCase
     }
     public function testGetResultList()
     {
+        $expected = [
+            [
+                "code" => "XX1",
+                "name" => "Paris"
+            ],
+            [
+                "code" => "XX2",
+                "name" => "Chartres"
+            ],
+            [
+                "code" => "XX3",
+                "name" => "Melun"
+            ]
+        ];
 
+        $result = $this->surveyManager->getSurveyList();
+        $this->assertSame(
+            array_multisort($expected),
+            array_multisort($result)
+        );
     }
+    public function testGetAggregatedSurvey() {
+        $expected = [
+            'What best sellers are available in your store?' => [
+                "Product 1" => 2,
+                "Product 2" => 1
+            ],
+            'Number of products?' => [
+                680
+            ],
+            '"What is the visit date?"' => [
+                '2017-06-09T00:00:00.000Z',
+                '2017-06-10T00:00:00.000Z'
+            ]
+        ];
+
+        $result = $this->surveyManager->getAggregatedSurvey('XX1');
+        $this->assertSame(
+            array_multisort($expected),
+            array_multisort($result)
+        );
+    }
+
 }
